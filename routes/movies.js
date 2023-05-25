a678922f52899bef39c5fd44e8e628fcf6b7e5d6
RedTengu <muratborokov@gmail.com>
2023-05-25 21:12:25 +0300

    feat: убрать удаление чужих карточек.

--- routes/movies.js
+++ routes/movies.js
@@ -2,9 +2,10 @@ const moviesRouter = require('express').Router();
 const { celebrate } = require('celebrate');
 
 const { getMyMovies, createMovie, deleteMovie } = require('../controllers/movies');
+const movieOwnerCheck = require('../middlewares/movieOwnerCheck');
 
 moviesRouter.get('/', getMyMovies);
 moviesRouter.post('/', createMovie);
-moviesRouter.delete('/:_id', deleteMovie);
+moviesRouter.delete('/:_id', movieOwnerCheck, deleteMovie);
 
 module.exports = moviesRouter;
